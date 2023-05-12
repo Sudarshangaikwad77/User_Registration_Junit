e244afae790b356fd17678e84d72a27aaff34f98
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-05-12 14:06:57 +0530

    added parameterised class with sample code

--- src/test/java/com/bl/Junit_Testing/UserTest.java
+++ src/test/java/com/bl/Junit_Testing/UserTest.java
@@ -1,65 +1,64 @@
 package com.bl.Junit_Testing;
 import org.junit.jupiter.api.*;
-public class UserTest
-{
-   UserRegistration firstName = new UserRegistration();
-
+public class UserTest {
     @BeforeAll
-    static void beforeall(){
+    static void beforeall() {
         System.out.println("Running Test Cases");
     }
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
 
         System.out.println("Running..........");
     }
 
     @AfterAll
-    static void afterAll(){
+    static void afterAll() {
         System.out.println("Test Cases Running Successfully!");
     }
 
     @Test
-    public void  checking_First() throws InvalidUserException{
-        boolean result = firstName.getFirstName("Sudarshan");
-        if (!result){
-            throw new InvalidUserException("Invalid FirstName");
-        }
-    }
-
-    @Test
-    public void checking_Last() throws InvalidUserException{
-       UserRegistration lastName = new UserRegistration();
-        boolean result = lastName.getLastName("Gaikwad");
-        if (!result){
-            throw new InvalidUserException("Invalid Lastname!");
-        }
+    public void checking_First() {
+        UserRegistration firstName = new UserRegistration();
+        boolean result = firstName.validateFirstName("Sudarshan");
+        Assertions.assertTrue(result);
     }
 
 
     @Test
-    public  void  checking_Email() throws  InvalidUserException {
-       UserRegistration email = new UserRegistration();
-        boolean result = email.getEmail("abc.xyz@bl.co.in");
-        if (!result) throw new InvalidUserException("Invalid Email");
+    public void checking_Last() {
+        UserRegistration lastName = new UserRegistration();
+        boolean result = lastName.validateLastName("Gaikwad");
+        Assertions.assertTrue(result);
     }
 
+
     @Test
-    public void checking_PhoneNum() throws InvalidUserException {
-        UserRegistration mobile = new UserRegistration();
-        boolean result = mobile.getMobile("91 9865678765");
+    public void checking_Email() {
+        UserRegistration email = new UserRegistration();
+        boolean result = email.validateEmail("abc.xyz@bl.co.in");
         Assertions.assertTrue(result);
     }
 
     @Test
-    public void checking_Password() throws InvalidUserException{
-        UserRegistration passwd = new UserRegistration();
-        boolean result = passwd.getPasswd("Sudarshan123@");
-        if (!result){
-            throw new InvalidUserException("Invalid Password");
-        }
+    public void checking_PhoneNum() {
+        UserRegistration mobile = new UserRegistration();
+        boolean result = mobile.validateMobile("91 8308622248");
+        Assertions.assertTrue(result);
     }
 
+    @Test
+    public void checking_Password() {
+        UserRegistration passwd = new UserRegistration();
+        boolean result = passwd.validatePassword("Sudarshan123@");
+        Assertions.assertTrue(result);
+    }
 
+    @Test
+    public void checking_WFirst() {
+        UserRegistration firstName = new UserRegistration();
+        boolean result = firstName.validateFirstName("sudarshan");
+        Assertions.assertTrue(result);
+    }
 }
+
